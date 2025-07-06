18913f64efdba02fd5537f51f66ec218f49fde4c
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2025-07-06 20:22:52 +0530

    Update outputs.tf

--- code/06-create-cloud-storage/outputs.tf
+++ code/06-create-cloud-storage/outputs.tf
@@ -1,4 +1,4 @@
 # Output variable: Google Cloud Storage Bucket
 output "storage_bucket_self_link" {
-  value = "${google_storage_bucket.byotf-testing-bucket.self_link}"
+  value = "${google_storage_bucket.byotf-yogita.self_link}"
 }
