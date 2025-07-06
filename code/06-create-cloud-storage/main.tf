136448bead099e02e757e3c1073f0dba21c43fbd
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2025-07-06 20:07:29 +0530

    Update main.tf

--- code/06-create-cloud-storage/main.tf
+++ code/06-create-cloud-storage/main.tf
@@ -1,11 +1,12 @@
 # Configure the Google Cloud provider
 provider "google" {
-  project = "cloud-appcenter-e2e-testing"
+  project = "solutions-yogita"
   region  = "us-central1"
 }
 
 # Create a Google Cloud Storage Bucket
-resource "google_storage_bucket" "terraform_state" {
+resource "google_storage_bucket" "byotf-testing-bucket" {
   name          = "${var.bucket_name}"
+  location      = "us"
   force_destroy = true
 }
