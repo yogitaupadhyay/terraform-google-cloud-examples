dc0b644bcec6d8dec96136d526b4781bd51e2016
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2025-07-06 15:47:12 +0530

    Update outputs.tf

--- code/04-one-webserver-with-vars/outputs.tf
+++ code/04-one-webserver-with-vars/outputs.tf
@@ -1,4 +1,4 @@
 # Output variable: Public IP address
 output "public_ip" {
-  value = "${google_compute_instance.example.network_interface}"
+  value = "${google_compute_instance.example.network_interface.0.access_config}"
 }
