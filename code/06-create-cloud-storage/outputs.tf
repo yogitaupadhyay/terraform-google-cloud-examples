861eed88d931058abc7557133a9d4049c7d60cdf
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2025-07-06 20:07:43 +0530

    Update outputs.tf

--- code/06-create-cloud-storage/outputs.tf
+++ code/06-create-cloud-storage/outputs.tf
@@ -1,4 +1,4 @@
 # Output variable: Google Cloud Storage Bucket
 output "storage_bucket_self_link" {
-  value = "${google_storage_bucket.terraform_state.self_link}"
+  value = "${google_storage_bucket.byotf-testing-bucket.self_link}"
 }
