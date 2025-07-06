3042cc04da50115d9e90d733ff7c33a2914cb384
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2025-07-06 20:22:37 +0530

    Update main.tf

--- code/06-create-cloud-storage/main.tf
+++ code/06-create-cloud-storage/main.tf
@@ -1,12 +1,13 @@
 # Configure the Google Cloud provider
 provider "google" {
-  project = "solutions-yogita"
+  project = "cloud-appcenter-e2e-testing"
   region  = "us-central1"
 }
 
 # Create a Google Cloud Storage Bucket
-resource "google_storage_bucket" "byotf-testing-bucket" {
+resource "google_storage_bucket" "byotf-yogita" {
   name          = "${var.bucket_name}"
   location      = "us"
   force_destroy = true
+  uniform_bucket_level_access = true
 }
