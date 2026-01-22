efa10589d5478e1a9c0caacd1977fb532533718f
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2026-01-22 21:25:53 +0530

    Add project_id variable to vars.tf
    Added a new variable for GCP project ID.

--- code/06-create-cloud-storage/vars.tf
+++ code/06-create-cloud-storage/vars.tf
@@ -3,3 +3,8 @@ variable "bucket_name" {
   description = "The name of the Google Cloud Storage bucket. Must be globally unique."
   default = "byotf-yogita"
 }
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID to deploy resources into."
+  # No default value - forces the user to provide one.
+}
